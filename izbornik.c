cbabc319cc53c1889ccf993b223ba8440c2c3d41
karlabrasnic <karla.brasnic@gmail.com>
2025-05-27 21:57:41 +0200

    izbornik.c

--- izbornik.c
+++ izbornik.c
@@ -1,23 +1,26 @@
-#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS 
 
 #include <stdio.h>
 #include "izbornik.h"
 
-void prikazi_izbornik(void) {
-	printf("\n--- VolleyCRO Izbornik ---\n");
-	printf("1. Dodaj igraca\n");
-	printf("2. Prikazi igrace\n");
-	printf("3. Azuriraj igraca\n");
-	printf("4. Obrisi igraca\n");
-	printf("5. Spremi u datoteku\n");
-	printf("6. Ucitaj iz datoteke\n");
-	printf("7. Izlaz\n");
-	printf("-------------------------\n");
-}
-
-IzbornikOpcije odaberi_opciju(void) {
-	int opcija;
-	printf("Odaberite opciju: ");
-	scanf("%d", &opcija);
-	return (IzbornikOpcije)opcija;
+int prikaziIzbornik() {
+    int izbor;
+    printf("\n--- VolleyCRO Izbornik ---\n");
+    printf("1. Ispisi igrace\n");
+    printf("2. Dodaj igraca\n");
+    printf("3. Azuriraj igraca\n");
+    printf("4. Obrisi igraca\n");
+    printf("5. Ispisi klubove\n");
+    printf("6. Dodaj klub\n");
+    printf("7. Azuriraj klub\n");
+    printf("8. Obrisi klub\n");
+    printf("9. Spremi podatke\n");
+    printf("10. Izlaz\n");
+    printf("Izaberite opciju: ");
+    if (scanf("%d", &izbor) != 1) {
+        printf("Neispravan unos!\n");
+        izbor = -1;
+        while (getchar() != '\n'); 
+    }
+    return izbor;
 }
