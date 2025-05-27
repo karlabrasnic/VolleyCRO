40e02cdcc79601c21193ee78a4f835666af49ef3
karlabrasnic <karla.brasnic@gmail.com>
2025-05-27 21:56:33 +0200

    datoteka.c

--- datoteka.c
+++ datoteka.c
@@ -1,47 +1,78 @@
-#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS 
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "datoteka.h"
 
-#define NAZIV_DATOTEKE "igraci.txt"
+int ucitaj_igrace(const char* filename, Igrac** igraci, int* broj) {
+	FILE* f = fopen("C:\\Users\\Fujitsu\\source\\repos\\volleyCRO2\\volleyCRO2\\x64\\Debug\\igraci.txt", "r");
+	if (!f) return 0;
 
-typedef struct {
-	int id;
-	char ime[30];
-	int broj;
-}; Igrac;
-
-int spremi_u_datoteku(const Igrac* igraci, int broj_igraca) {
-	FILE* f = fopen(NAZIV_DATOTEKE, "wb");
-	if (f == NULL) {
-		printf("Ne mogu otvoriti datoteku za pisanje.\n");
-		return 0;
+	*igraci = NULL;
+	*broj = 0;
+	Igrac temp;
+	while (fscanf(f, "%d %49s %49s %d %19s %d",
+		&temp.id, temp.ime, temp.prezime, &temp.godina_rodenja,
+		temp.pozicija, &temp.broj_kluba) == 6) {
+		Igrac* novi = realloc(*igraci, (*broj + 1) * sizeof(Igrac));
+		if (!novi) {
+			free(*igraci);
+			fclose(f);
+			return 0;
+		}
+		*igraci = novi;
+		(*igraci)[*broj] = temp;
+		(*broj)++;
 	}
-	fwrite(&broj_igraca, sizeof(int), 1, f);
-	fwrite(igraci, sizeof(Igrac), broj_igraca, f);
 	fclose(f);
 	return 1;
 }
 
-int ucitaj_iz_datoteke(Igrac** igraci, int* broj_igraca) {
-	FILE* f = fopen(NAZIV_DATOTEKE, "rb");
-	if (f == NULL) {
-		printf("Ne mogu otvoriti datoteku za citanje.\n");
-		return 0;
+int spremi_igrace(const char* filename, Igrac* igraci, int broj) {
+	FILE* f = fopen("C:\\Users\\Fujitsu\\source\\repos\\volleyCRO2\\volleyCRO2\\x64\\Debug\\igraci.txt", "w");
+	if (!f) return 0;
+	for (int i = 0; i<broj; i++) {
+		fprintf(f, "%d %s %s %d %s %d\n",
+		igraci[i].id, igraci[i].ime, igraci[i].prezime,
+		igraci[i].godina_rodenja, igraci[i].pozicija, igraci[i].broj_kluba);
+    }
+    fclose(f);
+    return 1;
+}
+
+int ucitaj_klubove(const char* filename, Klub** klubovi, int* broj) {
+	FILE* f = fopen("C:\\Users\\Fujitsu\\source\\repos\\volleyCRO2\\volleyCRO2\\x64\\Debug\\klubovi.txt", "r");
+	if (!f) return 0;
+
+	*klubovi = NULL;
+	*broj = 0;
+	Klub temp;
+	while (fscanf(f, "%d %99s %49s %d %d %d",
+		&temp.id, temp.naziv, temp.grad, &temp.godine_osnivanja,
+		&temp.broj_pobjeda, &temp.broj_poraza) == 6) {
+		Klub* novi = realloc(*klubovi, (*broj + 1) * sizeof(Klub));
+		if (!novi) {
+			free(*klubovi);
+			fclose(f);
+			return 0;
+		}
+		*klubovi = novi;
+		(*klubovi)[*broj] = temp;
+		(*broj)++;
 	}
-	else {
-		printf("Datoteka otvorena uspjesno. \n");
-	}
-	fread(broj_igraca, sizeof(int), 1, f);
-	Igrac* temp = realloc(*igraci, (*broj_igraca) * sizeof(Igrac));
-	if (temp == NULL && *broj_igraca > 0) {
-		fclose(f);
-		printf("Greska pri alokaciji memorije.\n");
-		return 0;
-	}
-	*igraci = temp;
-	fread(*igraci, sizeof(Igrac), *broj_igraca, f);
 	fclose(f);
 	return 1;
+}
+
+int spremi_klubove(const char* filename, Klub* klubovi, int broj) {
+	FILE* f = fopen("C:\\Users\\Fujitsu\\source\\repos\\volleyCRO2\\volleyCRO2\\x64\\Debug\\klubovi.txt", "w");
+	if (!f) return 0;
+	for (int i = 0; i<broj; i++) { 
+		fprintf(f, "%d %s %s %d %d %d\n",
+		klubovi[i].id, klubovi[i].naziv, klubovi[i].grad,
+		klubovi[i].godine_osnivanja, klubovi[i].broj_pobjeda, klubovi[i].broj_poraza);
+    }
+    fclose(f);
+    return 1;
 }
