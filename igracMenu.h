2a5b68ba4159b842b54a0a2d9fee5b58cf1834ee
karlabrasnic <karla.brasnic@gmail.com>
2025-05-28 17:43:54 +0200

    igracMenu.h

---
+++ igracMenu.h
@@ -0,0 +1,10 @@
+#ifndef IGRACMENU_H
+#define IGRACMENU_H
+
+#include "igrac.h"
+
+void unosIgracaMenu(Igrac** igraci, int* brojIgraca);
+void azurirajIgracaMenu(Igrac* igraci, int brojIgraca);
+void obrisiIgracaMenu(Igrac** igraci, int* brojIgraca);
+
+#endif
