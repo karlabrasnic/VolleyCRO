943f6a42a9763ab74e7a7f58377064cc29b074c6
karlabrasnic <karla.brasnic@gmail.com>
2025-05-28 17:42:51 +0200

    klubMenu.h

---
+++ klubMenu.h
@@ -0,0 +1,11 @@
+#ifndef KLUB_MENU_H
+#define KLUB_MENU_H
+
+#include "klub.h"
+
+
+void unosKlubaMenu(Klub** klubovi, int* brojKlubova);
+void azurirajKlubMenu(Klub* klubovi, int brojKlubova);
+void obrisiKlubMenu(Klub** klubovi, int* brojKlubova);
+
+#endif
