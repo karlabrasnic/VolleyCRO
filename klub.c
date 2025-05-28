3121da01e8ee00cacad6473018384d333dd2acf6
karlabrasnic <karla.brasnic@gmail.com>
2025-05-28 17:46:49 +0200

    klub.c

--- klub.c
+++ klub.c
@@ -1,57 +1,60 @@
-#define _CRT_SECURE_NO_WARNINGS 
+#define _CRT_SECURE_NO_WARNINGS
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "klub.h"
 
-void initKlubove(Klub** klubovi, int* broj_klubova) {
-	*klubovi = NULL;
-	*broj_klubova = 0;
+void init_klubove(Klub** klubovi, int* broj_klubova) {
+    *klubovi = NULL;
+    *broj_klubova = 0;
 }
 
-int dodajKlub(Klub** klubovi, int* broj_klubova, Klub novi) {
-	Klub* temp = realloc(*klubovi, (*broj_klubova + 1) * sizeof(Klub));
-	if (!temp) return 0;
-	*klubovi = temp;
-	(*klubovi)[*broj_klubova] = novi;
-	(*broj_klubova)++;
-	return 1;
+int dodaj_klub(Klub** klubovi, int* broj_klubova, Klub novi) {
+    Klub* temp = realloc(*klubovi, (*broj_klubova + 1) * sizeof(Klub));
+    if (!temp) return 0;
+
+    *klubovi = temp;
+    (*klubovi)[*broj_klubova] = novi;
+    (*broj_klubova)++;
+    return 1;
 }
 
-void ispisiKlubove(Klub* klubovi, int broj) {
-	for (int i = 0; i<broj; i++) { 
-		printf("ID: %d, Naziv: %s, Grad: %s, Godine osnivanja: %d, Pobjede: %d, Porazi: %d\n",
-		klubovi[i].id, klubovi[i].naziv, klubovi[i].grad,
-		klubovi[i].godine_osnivanja, klubovi[i].broj_pobjeda,
-		klubovi[i].broj_poraza);
-}
+void ispisi_klubove(Klub* klubovi, int broj) {
+    for (int i = 0; i < broj; i++) {
+        printf("ID: %d, Naziv: %s, Grad: %s, Godine osnivanja: %d, Pobjede: %d, Porazi: %d\n",
+            klubovi[i].id, klubovi[i].naziv, klubovi[i].grad,
+            klubovi[i].godine_osnivanja, klubovi[i].broj_pobjeda, klubovi[i].broj_poraza);
+    }
 }
 
-int pronadjiKlub(Klub* klubovi, int broj, int id) {
-	for (int i = 0; i<broj; i++) {
-		if (klubovi[i].id == id) return i;
+int pronadji_klub(Klub* klubovi, int broj, int id) {
+    for (int i = 0; i < broj; i++) {
+        if (klubovi[i].id == id)
+            return i;
     }
     return -1;
 }
 
-int azurirajKlub(Klub* klubovi, int broj, int id, Klub noviPodaci) {
-	int idx = pronadjiKlub(klubovi, broj, id);
-	if (idx == -1) return 0;
-	klubovi[idx] = noviPodaci;
-	return 1;
+int azuriraj_klub(Klub* klubovi, int broj, int id, Klub novi_podaci) {
+    int idx = pronadji_klub(klubovi, broj, id);
+    if (idx == -1) return 0;
+    klubovi[idx] = novi_podaci;
+    return 1;
 }
 
-int obrisiKlub(Klub** klubovi, int* broj, int id) {
-	int idx = pronadjiKlub(*klubovi, *broj, id);
-	if (idx == -1) return 0;
-	for (int i = idx; i < *broj - 1; i++) {
-		(*klubovi)[i] = (*klubovi)[i + 1];
-	}
-	(*broj)--;
-	Klub* temp = realloc(*klubovi, (*broj) * sizeof(Klub));
-	if (temp || *broj == 0) {
-		*klubovi = temp;
-	}
-	return 1;
+int obrisi_klub(Klub** klubovi, int* broj, int id) {
+    int idx = pronadji_klub(*klubovi, *broj, id);
+    if (idx == -1) return 0;
+
+    for (int i = idx; i < *broj - 1; i++) {
+        (*klubovi)[i] = (*klubovi)[i + 1];
+    }
+
+    Klub* temp = realloc(*klubovi, (*broj - 1) * sizeof(Klub));
+    if (!temp && *broj - 1 > 0) return 0;
+
+    *klubovi = temp;
+    (*broj)--;
+    return 1;
 }
