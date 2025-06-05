e3326647de7717ee14034c6321cba43b5e82e678
karlabrasnic <karla.brasnic@gmail.com>
2025-06-05 22:12:23 +0200

    izbornik.h

--- izbornik.h
+++ izbornik.h
@@ -1,19 +1,19 @@
 #ifndef IZBORNIK_H
 #define IZBORNIK_H
 
-enum Opcija {
-	KRAJ = 0,
-	ISPISI_IGRACE = 1,
-	DODAJ_IGRACA,
-	AZURIRAJ_IGRACA,
-	OBRISI_IGRACA,
-	ISPISI_KLUBOVE,
-	DODAJ_KLUB,
-	AZURIRAJ_KLUB,
-	OBRISI_KLUB,
-	SPREMI_PODATKE
-};
+typedef enum {
+    KRAJ = 0,
+    ISPISI_IGRACE = 1,
+    DODAJ_IGRACA,
+    AZURIRAJ_IGRACA,
+    OBRISI_IGRACA,
+    ISPISI_KLUBOVE,
+    DODAJ_KLUB,
+    AZURIRAJ_KLUB,
+    OBRISI_KLUB,
+    SPREMI_PODATKE
+} Opcija;
 
-int prikaziIzbornik();
+int prikazi_izbornik(void);
 
 #endif
