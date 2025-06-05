f588b882121cdfe2ce318b24677593b151d85880
karlabrasnic <karla.brasnic@gmail.com>
2025-06-05 23:41:50 +0200

    klub.h

--- klub.h
+++ klub.h
@@ -1,20 +1,25 @@
 #ifndef KLUB_H
 #define KLUB_H
 
+#define MAX_NAZIV 100
+#define MAX_GRAD 50
+
+
 typedef struct {
     int id;
-    char naziv[100];
-    char grad[100];
+    char naziv[MAX_NAZIV];
+    char grad[MAX_GRAD];
+    int godine_osnivanja;
     int broj_pobjeda;
     int broj_poraza;
 } Klub;
 
-Klub* ucitajKlubove(const char* nazivDatoteke, int* brojKlubova);
-void spremiKlubove(const char* nazivDatoteke, Klub* klubovi, int brojKlubova);
-Klub* dodajKlub(Klub* klubovi, int* brojKlubova);
-Klub* obrisiKlub(Klub* klubovi, int* brojKlubova);
-void ispisiKlubove(Klub* klubovi, int brojKlubova);
-void pretraziKlubove(Klub* klubovi, int brojKlubova);
-void sortirajKlubove(Klub* klubovi, int brojKlubova);
 
-#endif
+void init_klubove(Klub** klubovi, int* broj_klubova);
+int dodaj_klub(Klub** klubovi, int* broj_klubova, Klub novi);
+void ispisi_klubove(const Klub* klubovi, int broj);
+int pronadji_klub(const Klub* klubovi, int broj, int id);
+int azuriraj_klub(Klub* klubovi, int broj, int id, Klub novi_podaci);
+int obrisi_klub(Klub** klubovi, int* broj_klubova, int id);
+
+#endif 
