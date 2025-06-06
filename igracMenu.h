bec22fab5532aeb760cecbd22aa05f5d70c22870
karlabrasnic <karla.brasnic@gmail.com>
2025-06-06 08:54:54 +0200

    igracMenu.h

--- igracMenu.h
+++ igracMenu.h
@@ -4,7 +4,8 @@
 #include "igrac.h"
 
 void unos_igraca_menu(Igrac** igraci, int* broj_igraca);
-void azuriraj_igraca_menu(Igrac** igraci, int broj_igraca);
+void azuriraj_igraca_menu(Igrac* igraci, int broj_igraca);
 void obrisi_igraca_menu(Igrac** igraci, int* broj_igraca);
 
 #endif
+
