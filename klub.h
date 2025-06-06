670bb70ff8d451aca33149613a43a5ecbed2f389
karlabrasnic <karla.brasnic@gmail.com>
2025-06-06 09:47:43 +0200

    klub.h

--- klub.h
+++ klub.h
@@ -8,10 +8,17 @@ typedef struct {
     int id;
     char naziv[MAX_NAZIV];
     char grad[MAX_GRAD];
+    int godina_osnivanja;
 } Klub;
 
-int dodaj_klub(Klub** klubovi, int* broj_klubova, Klub novi);
-void ispisi_klubove(const Klub* klubovi, int broj_klubova);
+void init_klubove(Klub** klubovi, int* broj);
+int dodaj_klub(Klub** klubovi, int* broj, Klub novi);
+int obrisi_klub(Klub* klubovi, int* broj, int id);
+int azuriraj_klub(Klub* klubovi, int broj, int id, Klub novi);
+int usporedi_klubove(const void* a, const void* b);
+int usporedi_klub_po_id(const void* key, const void* elem);
+
 #endif
+
 
 
