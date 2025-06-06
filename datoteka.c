f5fb5a642def442902b771088e415f81f6564210
karlabrasnic <karla.brasnic@gmail.com>
2025-06-06 09:29:30 +0200

    datoteka.c

--- datoteka.c
+++ datoteka.c
@@ -2,126 +2,165 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <errno.h>
 #include "datoteka.h"
+#include "igrac.h"
+#include "klub.h"
 
-int ucitaj_igrace(const char* filename, Igrac** igraci, int* broj) {
-    FILE* f = safe_fopen(filename, "r");
-    if (!f) {
-        // perror is already u safe_fopen
+int ucitaj_igrace(const char* naziv_datoteke, Igrac** igraci, int* broj_igraca) {
+    if (!naziv_datoteke || !igraci || !broj_igraca) return 0;
+
+    FILE* fp = fopen(naziv_datoteke, "r");
+    if (!fp) {
+        perror("Ne mogu otvoriti datoteku igraci");
         return 0;
     }
 
-    *igraci = NULL;
-    *broj = 0;
     Igrac temp;
-    while (fscanf(f, "%d %49s %49s %d %19s %d",
-                  &temp.id, temp.ime, temp.prezime, &temp.godina_rodenja,
-                  temp.pozicija, &temp.broj_kluba) == 6) {
-        Igrac* novi = realloc(*igraci, (*broj + 1) * sizeof(Igrac));
-        if (!novi) {
-            free(*igraci);
-            *igraci = NULL;
-            *broj = 0;
-            safe_fclose(f);
-            fprintf(stderr, "Greska: Neuspjesna realokacija memorije za igrace.\n");
-            return 0;
+    int uspjeh = 1;
+
+    while (fscanf(fp, "%d %49s %49s %d %19s %d",
+                  &temp.id, temp.ime, temp.prezime,
+                  &temp.godina_rodenja, temp.pozicija, &temp.broj_kluba) == 6) {
+        if (!dodaj_igraca(igraci, broj_igraca, temp)) {
+            fprintf(stderr, "Greska alokacije igraca\n");
+            uspjeh = 0;
+            break;
         }
-        *igraci = novi;
-        (*igraci)[*broj] = temp;
-        (*broj)++;
     }
 
-    if (ferror(f)) {
-        perror("Greska prilikom citanja iz datoteke igraci");
-        free(*igraci);
-        *igraci = NULL;
-        *broj = 0;
-        safe_fclose(f);
+    if (ferror(fp)) {
+        perror("Greska pri citanju igraci");
+        uspjeh = 0;
+    }
+
+    fclose(fp);
+    return uspjeh;
+}
+
+int spremi_igrace(const char* naziv_datoteke, const Igrac* igraci, int broj_igraca) {
+    if (!naziv_datoteke || !igraci || broj_igraca <= 0) return 0;
+
+    FILE* fp = fopen(naziv_datoteke, "w");
+    if (!fp) {
+        perror("Ne mogu otvoriti datoteku igraci za pisanje");
         return 0;
     }
 
-    safe_fclose(f);
-    return 1;
-}
-
-int spremi_igrace(const char* filename, Igrac* igraci, int broj) {
-    FILE* f = safe_fopen(filename, "w");
-    if (!f) return 0;
-
-    for (int i = 0; i < broj; i++) {
-        if (fprintf(f, "%d %s %s %d %s %d\n",
-                    igraci[i].id, igraci[i].ime, igraci[i].prezime,
-                    igraci[i].godina_rodenja, igraci[i].pozicija, igraci[i].broj_kluba) < 0) {
-            perror("Greska pri pisanju u datoteku igraci");
-            safe_fclose(f);
-            return 0;
+    int uspjeh = 1;
+    for (int i = 0; i < broj_igraca; i++) {
+        if (fprintf(fp, "%d %s %s %d %s %d\n",
+            igraci[i].id, igraci[i].ime, igraci[i].prezime,
+            igraci[i].godina_rodenja, igraci[i].pozicija, igraci[i].broj_kluba) < 0) {
+            perror("Greska pri pisanju igraci");
+            uspjeh = 0;
+            break;
         }
     }
 
-    if (fclose(f) == EOF) {
+    if (fclose(fp) == EOF) {
         perror("Greska pri zatvaranju datoteke igraci");
-        return 0;
+        uspjeh = 0;
     }
-    return 1;
+
+    return uspjeh;
 }
 
-int ucitaj_klubove(const char* filename, Klub** klubovi, int* broj) {
-    FILE* f = safe_fopen(filename, "r");
-    if (!f) return 0;
+// -- klub --
 
-    *klubovi = NULL;
-    *broj = 0;
+int ucitaj_klubove(const char* naziv_datoteke, Klub** klubovi, int* broj_klubova) {
+    if (!naziv_datoteke || !klubovi || !broj_klubova) return 0;
+
+    FILE* fp = fopen(naziv_datoteke, "r");
+    if (!fp) {
+        perror("Ne mogu otvoriti datoteku klubovi");
+        return 0;
+    }
+
     Klub temp;
-    while (fscanf(f, "%d %99s %49s %d %d %d",
-                  &temp.id, temp.naziv, temp.grad, &temp.godine_osnivanja,
-                  &temp.broj_pobjeda, &temp.broj_poraza) == 6) {
-        Klub* novi = realloc(*klubovi, (*broj + 1) * sizeof(Klub));
-        if (!novi) {
-            free(*klubovi);
-            *klubovi = NULL;
-            *broj = 0;
-            safe_fclose(f);
-            fprintf(stderr, "Greska: Neuspjesna realokacija memorije za klubove.\n");
-            return 0;
+    int uspjeh = 1;
+
+    while (fscanf(fp, "%d %49s %49s",
+                  &temp.id, temp.naziv, temp.grad) == 3) {
+        if (!dodaj_klub(klubovi, broj_klubova, temp)) {
+            fprintf(stderr, "Greska alokacije kluba\n");
+            uspjeh = 0;
+            break;
         }
-        *klubovi = novi;
-        (*klubovi)[*broj] = temp;
-        (*broj)++;
     }
 
-    if (ferror(f)) {
-        perror("Greska prilikom citanja iz datoteke klubovi");
-        free(*klubovi);
-        *klubovi = NULL;
-        *broj = 0;
-        safe_fclose(f);
-        return 0;
+    if (ferror(fp)) {
+        perror("Greska pri citanju klubovi");
+        uspjeh = 0;
     }
 
-    safe_fclose(f);
-    return 1;
+    fclose(fp);
+    return uspjeh;
 }
 
-int spremi_klubove(const char* filename, Klub* klubovi, int broj) {
-    FILE* f = safe_fopen(filename, "w");
-    if (!f) return 0;
+int spremi_klubove(const char* naziv_datoteke, const Klub* klubovi, int broj_klubova) {
+    if (!naziv_datoteke || !klubovi || broj_klubova <= 0) return 0;
 
-    for (int i = 0; i < broj; i++) {
-        if (fprintf(f, "%d %s %s %d %d %d\n",
-                    klubovi[i].id, klubovi[i].naziv, klubovi[i].grad,
-                    klubovi[i].godine_osnivanja, klubovi[i].broj_pobjeda, klubovi[i].broj_poraza) < 0) {
-            perror("Greska pri pisanju u datoteku klubovi");
-            safe_fclose(f);
-            return 0;
+    FILE* fp = fopen(naziv_datoteke, "w");
+    if (!fp) {
+        perror("Ne mogu otvoriti datoteku klubovi za pisanje");
+        return 0;
+    }
+
+    int uspjeh = 1;
+    for (int i = 0; i < broj_klubova; i++) {
+        if (fprintf(fp, "%d %s %s\n",
+            klubovi[i].id, klubovi[i].naziv, klubovi[i].grad) < 0) {
+            perror("Greska pri pisanju klubovi");
+            uspjeh = 0;
+            break;
         }
     }
 
-    if (fclose(f) == EOF) {
+    if (fclose(fp) == EOF) {
         perror("Greska pri zatvaranju datoteke klubovi");
+        uspjeh = 0;
+    }
+
+    return uspjeh;
+}
+
+// Kopiranje datoteke - binarno
+int kopiraj_datoteku(const char* ulaz, const char* izlaz) {
+    if (!ulaz || !izlaz) return 0;
+
+    FILE* in = fopen(ulaz, "rb");
+    if (!in) {
+        perror("Ne mogu otvoriti ulaznu datoteku");
         return 0;
     }
-    return 1;
+
+    FILE* out = fopen(izlaz, "wb");
+    if (!out) {
+        perror("Ne mogu otvoriti izlaznu datoteku");
+        fclose(in);
+        return 0;
+    }
+
+    char buffer[4096];
+    size_t bytes;
+    int uspjeh = 1;
+
+    while ((bytes = fread(buffer, 1, sizeof(buffer), in)) > 0) {
+        if (fwrite(buffer, 1, bytes, out) != bytes) {
+            perror("Greska pri pisanju u izlaznu datoteku");
+            uspjeh = 0;
+            break;
+        }
+    }
+
+    if (ferror(in)) {
+        perror("Greska pri citanju ulazne datoteke");
+        uspjeh = 0;
+    }
+
+    fclose(in);
+    fclose(out);
+    return uspjeh;
 }
 
