6d0cc204caa5d492e670c5456d3d911b38befdac
karlabrasnic <karla.brasnic@gmail.com>
2025-06-06 09:50:47 +0200

    izbornik.h

--- izbornik.h
+++ izbornik.h
@@ -1,20 +1,8 @@
 #ifndef IZBORNIK_H
 #define IZBORNIK_H
 
-enum Opcije {
-    ISPISI_IGRACE = 1,
-    DODAJ_IGRACA,
-    AZURIRAJ_IGRACA,
-    OBRISI_IGRACA,
-    ISPISI_KLUBOVE,
-    DODAJ_KLUB,
-    AZURIRAJ_KLUB,
-    OBRISI_KLUB,
-    SPREMI_PODATKE,
-    KRAJ
-};
-
-int prikazi_izbornik(void);
+void prikazi_glavni_izbornik(void);
+int odaberi_opciju(int max_opcija);
 
 #endif
 
