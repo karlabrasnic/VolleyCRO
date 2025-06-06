a8f09a8e3d9a39bf5bcb3a809885177cc77f5eef
karlabrasnic <karla.brasnic@gmail.com>
2025-06-06 09:29:03 +0200

    datoteka.h

--- datoteka.h
+++ datoteka.h
@@ -4,10 +4,12 @@
 #include "igrac.h"
 #include "klub.h"
 
-int ucitaj_igrace(const char* filename, Igrac** igraci, int* broj);
-int spremi_igrace(const char* filename, Igrac* igraci, int broj);
+int ucitaj_igrace(const char* naziv_datoteke, Igrac** igraci, int* broj_igraca);
+int spremi_igrace(const char* naziv_datoteke, const Igrac* igraci, int broj_igraca);
 
-int ucitaj_klubove(const char* filename, Klub** klubovi, int* broj);
-int spremi_klubove(const char* filename, Klub* klubovi, int broj);
+int ucitaj_klubove(const char* naziv_datoteke, Klub** klubovi, int* broj_klubova);
+int spremi_klubove(const char* naziv_datoteke, const Klub* klubovi, int broj_klubova);
+
+int kopiraj_datoteku(const char* ulaz, const char* izlaz);
 
 #endif
