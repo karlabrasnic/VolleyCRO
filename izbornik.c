68d37ca2482b920df6a99cc158e7daeab63d5291
karlabrasnic <karla.brasnic@gmail.com>
2025-06-06 09:34:59 +0200

    izbornik.c

--- izbornik.c
+++ izbornik.c
@@ -1,29 +1,21 @@
-#define _CRT_SECURE_NO_WARNINGS
-
 #include <stdio.h>
 #include "izbornik.h"
 
 int prikazi_izbornik(void) {
-    int izbor;
-    printf("\n--- VolleyCRO Izbornik ---\n");
-    printf("1. Ispisi igrace\n");
-    printf("2. Dodaj igraca\n");
-    printf("3. Azuriraj igraca\n");
-    printf("4. Obrisi igraca\n");
-    printf("5. Ispisi klubove\n");
+    int opcija;
+    printf("\n-- Izbornik --\n");
+    printf("1. Ispis igrača\n");
+    printf("2. Dodaj igrača\n");
+    printf("3. Ažuriraj igrača\n");
+    printf("4. Obriši igrača\n");
+    printf("5. Ispis klubova\n");
     printf("6. Dodaj klub\n");
-    printf("7. Azuriraj klub\n");
-    printf("8. Obrisi klub\n");
+    printf("7. Ažuriraj klub\n");
+    printf("8. Obriši klub\n");
     printf("9. Spremi podatke\n");
-    printf("0. Izlaz\n");
-    printf("Izaberite opciju: ");
-
-    if (scanf("%d", &izbor) != 1) {
-        printf("Neispravan unos!\n");
-        izbor = -1;
-        while (getchar() != '\n');  // čisti buffer
-    }
-
-    return izbor;
+    printf("10. Izlaz\n");
+    printf("Odaberite opciju: ");
+    scanf("%d", &opcija);
+    return opcija;
 }
 
