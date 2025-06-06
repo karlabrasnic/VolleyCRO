1a8031d29a255f0da72405510f8077a47fefad85
karlabrasnic <karla.brasnic@gmail.com>
2025-06-06 09:51:30 +0200

    izbornik.c

--- izbornik.c
+++ izbornik.c
@@ -1,21 +1,26 @@
 #include <stdio.h>
 #include "izbornik.h"
 
-int prikazi_izbornik(void) {
-    int opcija;
-    printf("\n-- Izbornik --\n");
-    printf("1. Ispis igrača\n");
-    printf("2. Dodaj igrača\n");
-    printf("3. Ažuriraj igrača\n");
-    printf("4. Obriši igrača\n");
-    printf("5. Ispis klubova\n");
-    printf("6. Dodaj klub\n");
-    printf("7. Ažuriraj klub\n");
-    printf("8. Obriši klub\n");
-    printf("9. Spremi podatke\n");
-    printf("10. Izlaz\n");
+void prikazi_glavni_izbornik(void) {
+    printf("\n===========================================\n");
+    printf("   --- Dobrodošli u VolleyCRO aplikaciju! ---\n");
+    printf("===========================================\n");
+    printf("1. Upravljanje igračima\n");
+    printf("2. Upravljanje klubovima\n");
+    printf("3. Spremi podatke\n");
+    printf("4. Učitaj podatke\n");
+    printf("5. Izlaz\n");
     printf("Odaberite opciju: ");
-    scanf("%d", &opcija);
+}
+
+int odaberi_opciju(int max_opcija) {
+    int opcija;
+    int result = scanf("%d", &opcija);
+    while (result != 1 || opcija < 1 || opcija > max_opcija) {
+        while (getchar() != '\n'); // čišćenje buffera
+        printf("Neispravan unos. Ponovite: ");
+        result = scanf("%d", &opcija);
+    }
     return opcija;
 }
 
