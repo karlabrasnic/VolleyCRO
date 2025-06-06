d2bd99c00106b92c1ce00c9ba4f27f1e113a2f0e
karlabrasnic <karla.brasnic@gmail.com>
2025-06-06 09:00:04 +0200

    klubMenu.c

--- klubMenu.c
+++ klubMenu.c
@@ -6,55 +6,56 @@
 #include "klubMenu.h"
 #include "klub.h"
 
+#define CLEAR_INPUT_BUFFER() while (getchar() != '\n')
+
 void unos_kluba_menu(Klub** klubovi, int* broj_klubova) {
     Klub novi;
 
     printf("Unesite ID kluba: ");
     if (scanf("%d", &novi.id) != 1) {
         printf("Neispravan unos ID-a.\n");
-        while (getchar() != '\n');
+        CLEAR_INPUT_BUFFER();
         return;
     }
 
     printf("Unesite naziv kluba: ");
     if (scanf(" %99s", novi.naziv) != 1) {
         printf("Neispravan unos naziva.\n");
-        while (getchar() != '\n');
+        CLEAR_INPUT_BUFFER();
         return;
     }
 
     printf("Unesite grad: ");
     if (scanf(" %49s", novi.grad) != 1) {
         printf("Neispravan unos grada.\n");
-        while (getchar() != '\n');
+        CLEAR_INPUT_BUFFER();
         return;
     }
 
     printf("Unesite godine osnivanja: ");
     if (scanf("%d", &novi.godine_osnivanja) != 1) {
         printf("Neispravan unos godine osnivanja.\n");
-        while (getchar() != '\n');
+        CLEAR_INPUT_BUFFER();
         return;
     }
 
     printf("Unesite broj pobjeda: ");
     if (scanf("%d", &novi.broj_pobjeda) != 1) {
         printf("Neispravan unos broja pobjeda.\n");
-        while (getchar() != '\n');
+        CLEAR_INPUT_BUFFER();
         return;
     }
 
     printf("Unesite broj poraza: ");
     if (scanf("%d", &novi.broj_poraza) != 1) {
         printf("Neispravan unos broja poraza.\n");
-        while (getchar() != '\n');
+        CLEAR_INPUT_BUFFER();
         return;
     }
 
     if (dodaj_klub(klubovi, broj_klubova, novi)) {
         printf("Klub uspješno dodan.\n");
-    }
-    else {
+    } else {
         printf("Greška pri dodavanju kluba.\n");
     }
 }
@@ -65,7 +66,7 @@ void azuriraj_klub_menu(Klub* klubovi, int broj_klubova) {
     printf("Unesite ID kluba za ažuriranje: ");
     if (scanf("%d", &id) != 1) {
         printf("Neispravan unos ID-a.\n");
-        while (getchar() != '\n');
+        CLEAR_INPUT_BUFFER();
         return;
     }
 
@@ -81,42 +82,41 @@ void azuriraj_klub_menu(Klub* klubovi, int broj_klubova) {
     printf("Unesite novi naziv kluba: ");
     if (scanf(" %99s", novi.naziv) != 1) {
         printf("Neispravan unos naziva.\n");
-        while (getchar() != '\n');
+        CLEAR_INPUT_BUFFER();
         return;
     }
 
     printf("Unesite novi grad: ");
     if (scanf(" %49s", novi.grad) != 1) {
         printf("Neispravan unos grada.\n");
-        while (getchar() != '\n');
+        CLEAR_INPUT_BUFFER();
         return;
     }
 
     printf("Unesite novu godinu osnivanja: ");
     if (scanf("%d", &novi.godine_osnivanja) != 1) {
         printf("Neispravan unos godine osnivanja.\n");
-        while (getchar() != '\n');
+        CLEAR_INPUT_BUFFER();
         return;
     }
 
     printf("Unesite novi broj pobjeda: ");
     if (scanf("%d", &novi.broj_pobjeda) != 1) {
         printf("Neispravan unos broja pobjeda.\n");
-        while (getchar() != '\n');
+        CLEAR_INPUT_BUFFER();
         return;
     }
 
     printf("Unesite novi broj poraza: ");
     if (scanf("%d", &novi.broj_poraza) != 1) {
         printf("Neispravan unos broja poraza.\n");
-        while (getchar() != '\n');
+        CLEAR_INPUT_BUFFER();
         return;
     }
 
     if (azuriraj_klub(klubovi, broj_klubova, id, novi)) {
         printf("Klub uspješno ažuriran.\n");
-    }
-    else {
+    } else {
         printf("Greška pri ažuriranju kluba.\n");
     }
 }
@@ -127,14 +127,13 @@ void obrisi_klub_menu(Klub** klubovi, int* broj_klubova) {
     printf("Unesite ID kluba za brisanje: ");
     if (scanf("%d", &id) != 1) {
         printf("Neispravan unos ID-a.\n");
-        while (getchar() != '\n');
+        CLEAR_INPUT_BUFFER();
         return;
     }
 
     if (obrisi_klub(klubovi, broj_klubova, id)) {
         printf("Klub uspješno obrisan.\n");
-    }
-    else {
+    } else {
         printf("Klub nije pronađen.\n");
     }
 }
