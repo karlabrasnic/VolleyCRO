272ab66423c9558c3135e139b5395c4c2160a7c7
karlabrasnic <karla.brasnic@gmail.com>
2025-06-30 17:29:55 +0200

    igrac.h

--- igrac.h
+++ igrac.h
@@ -1,22 +1,21 @@
+#ifndef IGRAC_H
+#define IGRAC_H
+
 #define MAX_IME 50
 #define MAX_PREZIME 50
-
+#define MAX_KLUB 50
+#define MAX_POZICIJA 30
 
 typedef struct {
-    int id;                             
-    char ime[MAX_IME];               
-    char prezime[MAX_PREZIME];         
-    int godine;                       
-    int broj_odigranih_utakmica;    
-    int klub_id;                      
+    int id;
+    char ime[MAX_IME];
+    char prezime[MAX_PREZIME];
+    char klub[MAX_KLUB];
+    int godine;
+    char pozicija[MAX_POZICIJA];
 } Igrac;
 
+int usporedi_ime(const void* a, const void* b);
+int binarno_pretrazi_rekurzivno(const Igrac* niz, int lijevo, int desno, const char* ime);
 
-int dodaj_igraca(Igrac** igraci, int* broj_igraca, Igrac novi);
-int pronadji_igraca(const Igrac* igraci, int broj_igraca, int id);
-int azuriraj_igraca(Igrac* igraci, int broj_igraca, int id, Igrac novi_podaci);
-int obrisi_igraca(Igrac** igraci, int* broj_igraca, int id);
-void ispisi_igrace(const Igrac* igraci, int broj_igraca);
-
-#endif // IGRAC_H
-
+#endif 
