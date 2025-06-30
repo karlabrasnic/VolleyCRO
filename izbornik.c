e60e6b6bfc0f7fc8dc9e8f35cca6676b92f6982e
karlabrasnic <karla.brasnic@gmail.com>
2025-06-30 17:33:26 +0200

    izbornik.c

--- izbornik.c
+++ izbornik.c
@@ -5,25 +5,33 @@
 int prikazi_izbornik(void) {
     int izbor = -1;
 
-    printf("\n================ Izbornik =================\n");
-    printf("0 - Izlaz iz programa\n");
+    printf("\n===== Izbornik =====\n");
+    printf("0 - Izlaz\n");
     printf("1 - Ispisi igrace\n");
     printf("2 - Dodaj igraca\n");
     printf("3 - Azuriraj igraca\n");
     printf("4 - Obrisi igraca\n");
-    printf("5 - Ispisi klubove\n");
-    printf("6 - Dodaj klub\n");
-    printf("7 - Azuriraj klub\n");
-    printf("8 - Obrisi klub\n");
-    printf("9 - Spremi podatke\n");
-    printf("==========================================\n");
-    printf("Unesite opciju: ");
+    printf("5 - Spremi podatke\n");
+    printf("6 - Sortiraj po imenu\n");
+    printf("7 - Pronadi igraca po imenu\n");
+    printf("8 - Kopiraj datoteku igraca\n");
+    printf("9 - Obrisi datoteku igraca\n");
+    printf("10 - Preimenuj datoteku igraca\n");
+    printf("====================\n");
 
-    if (scanf("%d", &izbor) != 1) {
-        printf("Neispravan unos. Pokusajte ponovo.\n");
-        while (getchar() != '\n'); 
-        return -1;
+    while (1) {
+        printf("Unesite opciju: ");
+        if (scanf("%d", &izbor) != 1) {
+            printf("Neispravan unos, pokusajte ponovo.\n");
+            while (getchar() != '\n');
+            continue;
+        }
+        if (izbor < 0 || izbor > 10) {
+            printf("Opcija mora biti 0-10.\n");
+            continue;
+        }
+        while (getchar() != '\n');
+        break;
     }
-
     return izbor;
 }
