a6ad2e76cc1d4d9054841d64930c65a089a141e0
karlabrasnic <karla.brasnic@gmail.com>
2025-06-30 17:31:02 +0200

    datoteka.h

--- datoteka.h
+++ datoteka.h
@@ -2,13 +2,12 @@
 #define DATOTEKA_H
 
 #include "igrac.h"
-#include "klub.h"
 
-// Funkcije za rad s datotekama igrača i klubova
 int ucitaj_igrace(const char* ime_datoteke, Igrac** igraci, int* broj_igraca);
 int spremi_igrace(const char* ime_datoteke, const Igrac* igraci, int broj_igraca);
 
-int ucitaj_klubove(const char* ime_datoteke, Klub** klubovi, int* broj_klubova);
-int spremi_klubove(const char* ime_datoteke, const Klub* klubovi, int broj_klubova);
+int kopiraj_datoteku(const char* izvor, const char* odrediste);
+int obrisi_datoteku(const char* ime_datoteke);
+int preimenuj_datoteku(const char* stari_ime, const char* novi_ime);
 
-#endif
+#endif 
