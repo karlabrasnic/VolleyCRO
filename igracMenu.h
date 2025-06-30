b5cf9f04c07486b6e0da082516dad69c572ce699
karlabrasnic <karla.brasnic@gmail.com>
2025-06-30 17:31:37 +0200

    igracMenu.h

--- igracMenu.h
+++ igracMenu.h
@@ -1,14 +1,14 @@
-#ifndef IGRAC_MENU_H
-#define IGRAC_MENU_H
+#ifndef IGRACMENU_H
+#define IGRACMENU_H
 
 #include "igrac.h"
 
-
+void ispisi_igrace_menu(const Igrac* igraci, int broj_igraca);
 void unos_igraca_menu(Igrac** igraci, int* broj_igraca);
 void azuriraj_igraca_menu(Igrac* igraci, int broj_igraca);
 void obrisi_igraca_menu(Igrac** igraci, int* broj_igraca);
 
-#endif // IGRAC_MENU_H
+#endif
 
 
 
